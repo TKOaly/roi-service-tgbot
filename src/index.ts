d5333179c600bf8fc30d3479e7005023fbc68244
alehuo <aleksi.huotala@helsinki.fi>
2019-02-26 12:39:28 +0200

    Add error checking

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,8 @@ import TelegramBot from "node-telegram-bot-api";
 
 const jobFile = "jobs.json";
 
+if (!process.env.TELEGRAM_API_KEY) {
+  throw new Error("Please define a Telegram API key.");
+}
+
 const bot = new TelegramBot(process.env.TELEGRAM_API_KEY, { polling: true });
