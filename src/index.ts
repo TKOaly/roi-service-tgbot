98f34cb9ad02878c64d4aa8e4738afef2468ef37
alehuo <aleksi.huotala@helsinki.fi>
2019-02-27 16:56:44 +0200

    Add Winston logging, create app function for initializing the bot

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import fs from "fs";
 import TelegramBot from "node-telegram-bot-api";
+import { join } from "path";
+import { promisify } from "util";
+import { logger } from "./Logger";
+import { getJobs } from "./services/JobService";
 
 const jobFile = "jobs.json";
 
@@ -9,4 +14,30 @@ if (!process.env.TELEGRAM_API_KEY) {
   throw new Error("Please define a Telegram API key.");
 }
 
-const bot = new TelegramBot(process.env.TELEGRAM_API_KEY, { polling: true });
+const writeFileAsync = promisify(fs.writeFile);
+
+const app = async (telegramApiKey: string) => {
+  try {
+    // Sync jobs.json
+    logger.info("Fetching jobs", {
+      jobs_backend_url: process.env.JOBS_BACKEND_URL,
+    });
+    const jobs = await getJobs();
+    logger.info("Fetched jobs", {
+      jobs_backend_url: process.env.JOBS_BACKEND_URL,
+    });
+    const filepath = join("src", "data", jobFile);
+    logger.info("Writing jobs to file", { jobFile: filepath });
+    await writeFileAsync(filepath, JSON.stringify(jobs));
+    logger.info("Wrote jobs to file", { jobFile: filepath });
+
+    logger.info("Starting Telegram bot instance");
+    const bot = new TelegramBot(telegramApiKey, {
+      polling: true,
+    });
+  } catch (err) {
+    logger.error(err.toString());
+  }
+};
+
+app(process.env.TELEGRAM_API_KEY);
