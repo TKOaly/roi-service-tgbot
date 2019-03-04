de144b6fde97fd36e92a218ad27a9d24fea41ff1
alehuo <aleksi.huotala@helsinki.fi>
2019-03-04 14:20:41 +0200

    Add more tests

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,15 +1,24 @@
 import winston from "winston";
 
+const transports =
+  process.env.NODE_ENV === "test"
+    ? [
+        new winston.transports.Console({
+          format: winston.format.simple(),
+        }),
+      ]
+    : [
+        new winston.transports.File({ filename: "error.log", level: "error" }),
+        new winston.transports.File({ filename: "combined.log" }),
+        new winston.transports.Console({
+          format: winston.format.simple(),
+        }),
+      ];
+
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
-  transports: [
-    new winston.transports.File({ filename: "error.log", level: "error" }),
-    new winston.transports.File({ filename: "combined.log" }),
-    new winston.transports.Console({
-      format: winston.format.simple(),
-    }),
-  ],
+  transports,
 });
 
 export { logger };
--- test/FileUtils.test.ts
+++ test/FileUtils.test.ts
@@ -1,10 +1,22 @@
+process.env.NODE_ENV = "test";
+
 import { expect } from "chai";
+import fs from "fs";
 import "mocha";
 import { join } from "path";
-import { getChatIds, parseChatIds } from "../src/FileUtils";
+import {
+  fileExistsAsync,
+  getChatIds,
+  parseChatIds,
+  readFileAsync,
+  writeFileAsync,
+} from "../src/FileUtils";
 
-const validIds = join(__dirname, "testChatIds.json");
-const invalidIds = join(__dirname, "invalidChatIds.json");
+const validIds = join(__dirname, "files", "testChatIds.json");
+const invalidIds = join(__dirname, "files", "invalidChatIds.json");
+const testFile = join(__dirname, "files", "testFile.txt");
+const notExists = join(__dirname, "files", "notExists.txt");
+const tmpTestFile = join(__dirname, "files", "tmp.txt");
 
 describe("FileUtils", () => {
   describe("getChatIds()", () => {
@@ -53,4 +65,49 @@ describe("FileUtils", () => {
       done();
     });
   });
+  describe("readFileAsync()", () => {
+    it("Reads file correctly", async () => {
+      const res = await readFileAsync(testFile);
+      expect(res.toString()).to.equal("Hello World\nTest 123");
+    });
+  });
+  describe("writeFileAsync()", () => {
+    beforeEach((done) => {
+      // Setup (native funcs)
+      setupWrite(tmpTestFile);
+      done();
+    });
+    afterEach((done) => {
+      setupWrite(tmpTestFile);
+      done();
+    });
+    it("Writes to file correctly", async () => {
+      const data = "Hello World\nTest 123";
+      // Write asynchronously
+      await writeFileAsync(tmpTestFile, data);
+      // Read synchronously (native func)
+      const res = fs.readFileSync(tmpTestFile);
+      expect(res.toString()).to.equal(data);
+    });
+  });
+  describe("fileExistsAsync()", () => {
+    it("Returns true if file exists", async () => {
+      const exists = await fileExistsAsync(testFile);
+      expect(exists).to.equal(true);
+    });
+    it("Returns false if file does not exists", async () => {
+      const exists = await fileExistsAsync(notExists);
+      expect(exists).to.equal(false);
+    });
+  });
 });
+
+/**
+ * Deletes a file if found on disk.
+ * @param fileName Filename
+ */
+const setupWrite = (fileName: fs.PathLike) => {
+  if (fs.existsSync(fileName)) {
+    fs.unlinkSync(fileName);
+  }
+};
