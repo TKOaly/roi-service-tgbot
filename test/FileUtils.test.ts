4c636ad640f484616ac9d7c537f6e03c191f4b0a
alehuo <aleksi.huotala@helsinki.fi>
2019-03-04 13:47:49 +0200

    Create template for jobDifference() tests

--- test/FileUtils.test.ts
+++ test/FileUtils.test.ts
@@ -7,24 +7,50 @@ const validIds = join(__dirname, "testChatIds.json");
 const invalidIds = join(__dirname, "invalidChatIds.json");
 
 describe("FileUtils", () => {
-  it("Returns an empty array on a malformed ChatID file", async () => {
-    const chatIds = await getChatIds(invalidIds);
-    expect(chatIds).to.eql([]);
+  describe("getChatIds()", () => {
+    it("Returns an empty array on a malformed Chat ID file", async () => {
+      const chatIds = await getChatIds(invalidIds);
+      expect(chatIds).to.eql([]);
+    });
+    it("Returns an array of Chat IDs with the correct file", async () => {
+      const chatIds = await getChatIds(validIds);
+      expect(chatIds).to.eql(["12345", "67890"]);
+    });
   });
-  it("Returns an array of Chat IDs with the correct file", async () => {
-    const chatIds = await getChatIds(validIds);
-    expect(chatIds).to.eql(["12345", "67890"]);
+  describe("parseChatIds()", () => {
+    it("Parses Chat ID's correctly #1", async () => {
+      const chatIds = parseChatIds(JSON.parse('["12345", "67890"]'));
+      expect(chatIds).to.eql(["12345", "67890"]);
+    });
+    it("Parses Chat ID's correctly #1", async () => {
+      const chatIds = parseChatIds(JSON.parse("12345"));
+      expect(chatIds).to.eql([]);
+    });
+    it("Parses Chat ID's correctly #3", async () => {
+      const chatIds = parseChatIds(JSON.parse("[1, 2, 3, 4, 5]"));
+      expect(chatIds).to.eql([]);
+    });
   });
-  it("Parses Chat ID's correctly #1", async () => {
-    const chatIds = parseChatIds(JSON.parse('["12345", "67890"]'));
-    expect(chatIds).to.eql(["12345", "67890"]);
-  });
-  it("Parses Chat ID's correctly #1", async () => {
-    const chatIds = parseChatIds(JSON.parse("12345"));
-    expect(chatIds).to.eql([]);
-  });
-  it("Parses Chat ID's correctly #3", async () => {
-    const chatIds = parseChatIds(JSON.parse("[1, 2, 3, 4, 5]"));
-    expect(chatIds).to.eql([]);
+  describe("jobDifference()", () => {
+    it("If a new job posting is added, returns the new job", (done) => {
+      // TODO: Do test
+      done();
+    });
+    it("If multiple new job postings are added, returns the new jobs", (done) => {
+      // TODO: Do test
+      done();
+    });
+    it("If a job is modified, returns an empty array", (done) => {
+      // TODO: Do test
+      done();
+    });
+    it("If a job is removed, returns an empty array", (done) => {
+      // TODO: Do test
+      done();
+    });
+    it("If the fetched data is the same as the locally stored, returns an empty array", (done) => {
+      // TODO: Do test
+      done();
+    });
   });
 });
