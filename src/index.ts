d02ef5db132e3d9dd2887cff70b3853b742b6311
alehuo <aleksi.huotala@helsinki.fi>
2019-03-06 12:37:18 +0200

    Modify app structure to run a job check every monday, scheduled with cron

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import cron from "node-cron";
 dotenv.config();
 
 import TelegramBot from "node-telegram-bot-api";
@@ -17,16 +18,21 @@ if (!process.env.TELEGRAM_API_KEY) {
   throw new Error("Please define a Telegram API key.");
 }
 
+const everyMonday = "0 12 * * MON";
+const everyMinute = "* * * * *";
+
 const app = async (telegramApiKey: string) => {
+  logger.info("app(): Starting Telegram bot", {
+    NODE_ENV: process.env.NODE_ENV,
+  });
   try {
     // Sync jobs.json
     await fetchNewJobPostings(jobFile);
-
-    logger.info("app(): Starting Telegram bot instance");
     const bot = new TelegramBot(telegramApiKey, {
       polling: true,
     });
 
+    // Returns the Chat ID of the user.
     bot.onText(/^\/getchatid$/, (msg, match) => {
       const chatId = msg.chat.id;
       bot.sendMessage(chatId, "Your ChatID is: " + chatId);
@@ -34,6 +40,7 @@ const app = async (telegramApiKey: string) => {
 
     // For debugging
     if (process.env.NODE_ENV === "development") {
+      // Sends a test broadcast
       bot.onText(/^\/testbroadcast$/, (msg, match) => {
         const chatId = msg.chat.id;
         bot.sendMessage(
@@ -51,52 +58,44 @@ const app = async (telegramApiKey: string) => {
       logger.warn("app(): Chat ID file does not exist", { chatIdFile });
     } else {
       logger.info("app(): Starting Chat ID check interval");
-      // Set job check interval
-      setInterval(async () => {
-        logger.info("app(): Reading chat IDs to broadcast");
-        // Checks for updated jobs
-        const newJobs = await getNewJobPostings(jobFile);
-        // If there are new job postings available
-        if (newJobs.length > 0) {
-          await Promise.all(
-            newJobs.map(async (newJob) => {
-              logger.info("app(): Detected new job", {
-                title: newJob.title,
-                company: newJob.company,
-                url: newJob.url,
-              });
-              const chatIds = await getChatIds(chatIdFile);
-              if (chatIds.length > 0) {
-                return chatIds.map((chatId) => {
-                  logger.info("app(): Sending job info to chat", { chatId });
-                  const canApply =
-                    newJob.end !== null
-                      ? "Apply before " + newJob.end
-                      : "Applications accepted until further notice";
-                  const jobUrl = "https://jobs.tko-aly.fi/jobs/" + newJob.id;
-                  return bot.sendMessage(
-                    chatId,
-                    `*New job: ${newJob.company.name} - ${
-                      newJob.title
-                    }*\r\n${canApply}\r\n${jobUrl}`,
-                    {
-                      parse_mode: "Markdown",
-                    },
-                  );
-                });
-              }
-            }),
-          );
-        } else {
-          logger.info("app(): No new jobs at the moment.");
-        }
-        // Fetch new job postings (and save them locally)
-        await fetchNewJobPostings(jobFile);
-      }, 30000);
+      const expression =
+        process.env.NODE_ENV === "production" ? everyMonday : everyMinute;
+      logger.info("app(): Setting Cronjob expression", { expression });
+      // Set job check interval (jobCron is asynchronous)
+      cron.schedule(expression, async () => await scheduledTask(bot));
     }
   } catch (err) {
     logger.error(err.toString());
   }
+};
+
+const scheduledTask = async (bot: TelegramBot) => {
+  logger.info("scheduledTask(): Starting");
+  // Checks for updated jobs
+  const newJobs = await getNewJobPostings(jobFile);
+  // If there are new job postings available
+  if (newJobs.length > 0) {
+    // Get available Chat IDs
+    logger.info("scheduledTask(): Reading chat IDs to broadcast");
+    const chatIds = await getChatIds(chatIdFile);
+    // If there are chats that are configured, map them through
+    if (chatIds.length > 0) {
+      logger.info(`scheduledTask(): Detected ${newJobs.length} new jobs`);
+      // Broadcast new jobs to every chat that has been configured in the chats.json
+      await Promise.all(
+        chatIds.map((chatId) => {
+          logger.info("scheduledTask(): Sending job info to chat", { chatId });
+          return bot.sendMessage(chatId, generateMessage(newJobs), {
+            parse_mode: "Markdown",
+          });
+        }),
+      );
+    }
+  } else {
+    logger.info("scheduledTask(): No new jobs at the moment.");
+  }
+  // Fetch new job postings (and save them locally)
+  await fetchNewJobPostings(jobFile);
 };
 
 app(process.env.TELEGRAM_API_KEY);
